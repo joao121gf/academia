ada365e223fe9a341329f462aafbc07d7495576a
João Giovani <giovannirs957@gmail.com>
2023-05-20 15:33:33 -0300

    Finalizando design menu mobile

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,32 +1,39 @@
 export default function menuFunc() {
   window.addEventListener("load", () => {
-    const botao = document.querySelector('[data-button="fecha"]')
+    const botao = document.querySelector('[data-button="fecha"]');
     const menuMobile = document.querySelector("[data-menu]");
-    const botaoAbre = document.querySelector('[data-button="abre"]')
-    const mobileOff = document.querySelector('.mobile-off')
-  
-  
-  
-  
-  
-    botao.addEventListener('click', abreMenu)
-    function abreMenu(){
-      console.log(menuMobile)
-      menuMobile.style.display = 'none'
-  
-      menuMobile.classList.toggle('aberto')
-      if (menuMobile.classList.contains('aberto')){
-        menuMobile.style.display = 'block'
-        botaoAbre.style.display = 'none'
-        mobileOff.style.display = 'none'
+    const botaoAbre = document.querySelector('[data-button="abre"]');
+    const mobileOff = document.querySelector(".mobile-off");
+    botao.style.left = "70%";
+    botao.style.top = "21px";
+
+    botao.addEventListener("click", abreMenu);
+    function abreMenu() {
+      
+      menuMobile.style.display = "none";
+      menuMobile.classList.toggle("aberto");
+
+      
+      console.log(botao)
+
+      if (menuMobile.classList.contains("aberto")) {
+        menuMobile.style.display = "block";
+        botaoAbre.style.display = "none";
+        mobileOff.style.display = "none";
 
 
+        botao.style.left = "70%";
+        botao.style.top = "6%";
 
-      }else{
-        menuMobile.style.display = 'none'
+      } else {
+        botao.style.left = "70%";
+        botao.style.top = "21px";
+        // menuMobile.style.display = 'none'
       }
-      console.log(menuMobile)
-
     }
-  })
+    if (menuMobile.classList.contains('anima')){
+        console.log('oi')
+    }
+    console.log(menuMobile.classList.contains('anima'))
+  });
 }
