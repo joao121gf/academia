2366b4b29ef27dc6329b7aef655d1acda6792711
João Giovani <giovannirs957@gmail.com>
2023-05-20 14:03:31 -0300

    Ajustando menu mobile

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,40 +1,32 @@
 export default function menuFunc() {
-    const itensMenu = document.querySelectorAll(".menu li");
-    const linha = document.querySelector(".linha");
-    const home = document.querySelector('.menu li')
-    const homeWindow = home.getBoundingClientRect();
-    const posXHome = homeWindow.top + window.scrollX
-    const posYHome = homeWindow.left + window.scrollY
-    linha.style.left = posYHome + 'px';
-    linha.style.top = (posXHome + 30) + 'px';
-
-
-    console.log(homeWindow)
-
-
-    // Define a posição inicial e oculta a linha
-    linha.style.position = 'absolute';
-
+  window.addEventListener("load", () => {
+    const botao = document.querySelector('[data-button="fecha"]')
+    const menuMobile = document.querySelector("[data-menu]");
+    const botaoAbre = document.querySelector('[data-button="abre"]')
+    const mobileOff = document.querySelector('.mobile-off')
   
-    for (let i = 0; i < itensMenu.length; i++) {
-      itensMenu[i].addEventListener("click", chamaSeta);
-    }
   
-    function chamaSeta(event) {
-      const itemAtual = document.querySelector(".seta");
-      if (itemAtual) {
-        itemAtual.classList.remove("seta");
+  
+  
+  
+    botao.addEventListener('click', abreMenu)
+    function abreMenu(){
+      console.log(menuMobile)
+      menuMobile.style.display = 'none'
+  
+      menuMobile.classList.toggle('aberto')
+      if (menuMobile.classList.contains('aberto')){
+        menuMobile.style.display = 'block'
+        botaoAbre.style.display = 'none'
+        mobileOff.style.display = 'none'
+
+
+
+      }else{
+        menuMobile.style.display = 'none'
       }
-      this.classList.add("seta");
-      if (this.classList.contains("seta")) {
-        const liWindow = this.getBoundingClientRect();
-        const posicaoY = liWindow.left + window.scrollY;
-        const posicaoX = liWindow.top + window.scrollX;
-  
-        linha.style.left = posicaoY + 'px';
-        linha.style.top = (posicaoX + 30) + 'px';
+      console.log(menuMobile)
 
-      }
     }
-  }
-  
+  })
+}
