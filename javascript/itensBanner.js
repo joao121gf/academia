bfbd54dc58de244cd644413ce55076b2b52bba0f
João Giovani <giovannirs957@gmail.com>
2023-06-08 11:22:11 -0300

    Criando animacão no banner principal

--- javascript/itensBanner.js
+++ javascript/itensBanner.js
@@ -1,7 +1,15 @@
-
 export default function itensBanner() {
   window.addEventListener("load", () => {
-    console.log('ok')
+    const bannerPai = document.querySelector(".banner");
+    const filhosBanner = bannerPai.children;
+    const arrayFilhos = Array.from(filhosBanner);
+
+
+    arrayFilhos.forEach((items, index) => {
+      items.style.opacity = '0'
+      setTimeout(() => {
+        items.classList.add("sobe");
+      }, (index + 1) * 500);
+    });
   });
 }
-
